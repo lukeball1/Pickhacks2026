7e7a380910d640b1ec82236db38cd4c22f20d446
ewolf <elijahwolf05@icloud.com>
2026-02-28 19:35:02 -0600

    added color coordination for map pins

--- client/src/components/mapPin/mapPin.jsx
+++ client/src/components/mapPin/mapPin.jsx
@@ -1,19 +1,34 @@
 import {AdvancedMarker, Marker, Pin, useAdvancedMarkerRef, useMarkerRef} from '@vis.gl/react-google-maps';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function MapPin({pothole, onClick}) {
   const [markerRef, marker] = useAdvancedMarkerRef()
+  const [backgroundColor, setBackgroundColor] = useState('hsl(0 100% 50%)')
 
   useEffect(() => {
+    // const risk_score = (pothole['size']['width_cm'] * pothole['size']['depth_cm']) ** (1/2)
+    const risk_score = Math.sqrt(pothole['size']['width_cm'] * pothole['size']['depth_cm'])
+    console.log(pothole['size']['width_cm'], pothole['size']['depth_cm'])
+
+    console.log("risk: ", risk_score)
+
+    const hue = Math.round(180 - (risk_score * 12))
+
+    console.log(hue)
+
+    const color = 'hsl(' + ((hue > 0) ? hue : 0) + ' 100% 50%)'
+    console.log(color)
+    setBackgroundColor(color)
+    
     if (!marker) { 
       return;
     }
 
-  }, [marker])
+  }, [marker, backgroundColor])
 
   return (
     <AdvancedMarker ref={markerRef} position={{ lat: pothole['location']['coordinates'][1], lng: pothole['location']['coordinates'][0] }} onClick={() => {onClick()} }>
-      <Pin background={'#FF0000'} glyphColor={'#000'} borderColor={'#000'} />
+      <Pin background={backgroundColor} glyphColor={'#000'} borderColor={'#000'} />
     </AdvancedMarker>
   )
 }
